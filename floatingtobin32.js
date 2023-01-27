7317bd3a50d90ca5c89cff8e9bf12e64231ced52
redassser <redryan890@gmail.com>
2023-01-27 16:47:42 -0500

    added hex, real, value to 32bit

--- floatingtobin32.js
+++ floatingtobin32.js
@@ -10,16 +10,27 @@ function FloatToBin32() {
         if(byte.length<8) {byte = ("0".repeat(8-byte.length)).concat(byte)}
         binbytes.push(byte);
     });
+    var twomansum = 0;
+    for(let i=9;i<32;i++) {
+        twomansum += binbytes.join("")[i]*Math.pow(2, 8-i);
+    }
     var value = {
         binary: binbytes.join(""),
         sign: binbytes[0][0],
         exponent: binbytes.join("").slice(1,9),
         mantissa: binbytes.join("").slice(9),
+        twoexp: (parseInt(binbytes.join("").slice(1,9), 2)-127),
+        twoman: 1+twomansum
     }
-    
+
     document.getElementById("sign").innerHTML = value.sign;
     document.getElementById("exp").innerHTML = value.exponent;
     document.getElementById("man").innerHTML = value.mantissa;
     document.getElementById("bin").innerHTML = value.binary;
+    document.getElementById("signv").innerHTML = (value.sign==0) ? '+' : '-';
+    document.getElementById("expv").innerHTML = (inp!=0) ? `2<sup>${value.twoexp}</sup>` : "2<sup>0</sup>";
+    document.getElementById("manv").innerHTML = (inp!=0) ? value.twoman : "0";
+    document.getElementById("real").innerHTML = (inp!=0) ? value.twoman*Math.pow(2, value.twoexp): "0";
+    document.getElementById("hex").innerHTML = (inp!=0) ? "0x"+parseInt(value.binary,2).toString(16) : "0x00000000";
     return value;
 }
