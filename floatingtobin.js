230a824a0f0c2e2858be40da087e79e11a22f268
redassser <redryan890@gmail.com>
2023-01-28 20:03:44 -0500

    some comments

--- floatingtobin.js
+++ floatingtobin.js
@@ -1,15 +1,26 @@
 function FloatToBin32() {
-    const inp = document.getElementById("inp").value
-    var Binary = new ArrayBuffer(4);
-    new DataView(Binary).setFloat32(0, inp);
-    const bytes = new Uint8Array(Binary);
-    binbytes = [];
+    const inp = document.getElementById("inp").value //Get the number
 
+    //Getting the binary from a float in JS is not very simple 
+
+    //arraybuffer creates an array of bytes of size 4; 4 bytes = 32 bits; not directly writable/readable
+    var Binary = new ArrayBuffer(4); // -> [00000000, 00000000, 00000000, 00000000]
+
+    //Dataview lets us write to the array buffer 
+    //setfloat32 method for dataview stores our signed float in the buffer
+    new DataView(Binary).setFloat32(0, inp); //For example if inp=64 -> [01000010, 10000000, 00000000, 00000000]
+
+    //uint8array makes an arraybufferview of the buffer so we can access the directly read/writable array of bytes (as base 10 ints);
+    const bytes = new Uint8Array(Binary); // inp=64 -> [01000010, 10000000, 00000000, 00000000] -> [66, 128, 0, 0]
+
+    //Now we transfer to a readable binary array
+    binbytes = [];
     bytes.forEach(byte => {
-        byte = (byte >>> 0).toString(2);
-        if(byte.length<8) {byte = ("0".repeat(8-byte.length)).concat(byte)}
+        byte = byte.toString(2); //converts base 10 into base 2
+        if(byte.length<8) {byte = ("0".repeat(8-byte.length)).concat(byte)} //makes sure each binary number is 8 digits
         binbytes.push(byte);
     });
+    //Decode the binary from the mantissa into a fraction
     var twomansum = 0;
     for(let i=9;i<32;i++) {
         twomansum += binbytes.join("")[i]*Math.pow(2, 8-i);
@@ -23,6 +34,7 @@ function FloatToBin32() {
         twoman: 1+twomansum
     }
 
+    //plenty of values to change
     document.getElementById("sign").innerHTML = value.sign;
     document.getElementById("exp").innerHTML = value.exponent;
     document.getElementById("man").innerHTML = value.mantissa;
@@ -34,6 +46,7 @@ function FloatToBin32() {
     document.getElementById("hex").innerHTML = (inp!=0) ? "0x"+parseInt(value.binary,2).toString(16) : "0x00000000";
     return value;
 }
+//Practically the same except with a few minor changes
 function FloatToBin64() {
     const inp = document.getElementById("dinp").value
     var Binary = new ArrayBuffer(8);
